fdc0984529bc16eff1befdbff7e98ea612992545
Alien0427 <devanandithav@gmail.com>
2025-05-20 18:58:42 +0530

    Fix PYTHONPATH and import for tests in CI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,9 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 from app import app
 import json
-import os
 
 @pytest.fixture
 def client():
